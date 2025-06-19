a7b2e3b6ba26ec4c22247c2d39b917bd262094be
juosito <juosito@gmail.com>
2025-06-19 12:57:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-from fastapi import FastAPI
-import requests
-import os
-
-app = FastAPI()
-
-ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
-
-@app.get("/shipments")
-def get_shipments():
-    if not ACCESS_TOKEN:
-        return {"error": "Access token no definido."}
-
-    try:
-        url = "https://api.mercadolibre.com/marketplace/shipments/search"
-        headers = {
-            "Authorization": f"Bearer {ACCESS_TOKEN}"
-        }
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        return data
-    except Exception as e:
-        return {"error": str(e)}
+@app.get("/get-token")
+def get_token():
+    return {"access_token": refresh_access_token()}
