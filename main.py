6f66c624e667c6b651b557a5bcb2534cd8c1c554
juosito <juosito@gmail.com>
2025-06-19 14:48:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import requests
+import os
+
 @app.get("/get-token")
 def get_token():
-    return {"access_token": refresh_access_token()}
+    url = "https://api.mercadolibre.com/oauth/token"
+    payload = {
+        "grant_type": "refresh_token",
+        "client_id": os.getenv("CLIENT_ID"),
+        "client_secret": os.getenv("CLIENT_SECRET"),
+        "refresh_token": os.getenv("REFRESH_TOKEN_NARVAJA")
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    
+    response = requests.post(url, data=payload, headers=headers)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": response.text}
